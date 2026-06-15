8709619d7264e3c0979662f3be7d69ea47594f9b
Babatunde Adeoti <babaolu98@gmail.com>
2026-06-15 07:46:50 +0100

    build: globally ignore and untrack services/orchestrator build-native directory

--- services/orchestrator/build-native/CMakeFiles/orchestrator-server.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for orchestrator-server.
-# This may be replaced when dependencies are built.
--- services/orchestrator/build-native/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o.d
+++
@@ -1,4 +0,0 @@
-_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o: \
- /home/itunz/Work/AI_work/Poot/services/orchestrator/build-native/_deps/catch2-src/src/catch2/internal/catch_result_type.cpp \
- /usr/include/stdc-predef.h \
- /home/itunz/Work/AI_work/Poot/services/orchestrator/build-native/_deps/catch2-src/src/catch2/../catch2/internal/catch_result_type.hpp
